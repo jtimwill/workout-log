5dac852117f3e6b292897bdeb3bd4c0f7e370ad3
Tim Williams <jtimwill@gmail.com>
2020-12-03 14:58:02 -0500

    Add spinner to homepage

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
 import { login } from '../services/authService';
 import Lottie from 'react-lottie';
 import * as animationData from './squat.json';
+import { getMuscles } from '../services/muscleService.js';
+import Spinner from './reusable/spinner';
 
-const HomePage = () => {
-  const defaultOptions = {
+
+
+
+class HomePage extends Component {
+  state = {
+    api_response: false
+  };
+
+  defaultOptions = {
     loop: true,
     autoplay: true, 
     animationData: animationData.default,
@@ -14,34 +23,43 @@ const HomePage = () => {
     }
   };
 
-  async function exampleUserLogin() {
+  async componentDidMount() {
+    await getMuscles();
+    this.setState({ api_response: true });
+  }
+
+
+  async exampleUserLogin() {
     await login("adam@example.com", "123456");
     window.location = "/users/me/show";
   }
-  return(
-    <React.Fragment>
-      <div className="jumbotron custom-center">
-        <h1 className="display-4">Welcome to WorkoutLog!</h1>
-        <Lottie options={defaultOptions} height={350} width={350}/>
-        <p className="lead">
-          This project provides users with a simple interface for creating,
-          reviewing, updating and deleting workouts.
-        </p>
-        <hr className="my-4"/>
-        <Link
-          to="/users/new"
-          className="btn btn-danger btn-lg custom-right-margin custom-add-margin-top">
-          Register New Account
-        </Link>
-        <button
-          onClick={exampleUserLogin}
-          className="btn btn-info btn-lg custom-add-margin-top">
-          View Example Account
-        </button>
-      </div>
-      <div className="custom-flex-grow-column"/>
-    </React.Fragment>
-  );
-};
+
+  render() {
+    return(
+      <Spinner ready={this.state.api_response}>
+        <div className="jumbotron custom-center">
+          <h1 className="display-4">Welcome to WorkoutLog!</h1>
+          <Lottie options={this.defaultOptions} height={350} width={350}/>
+          <p className="lead">
+            This project provides users with a simple interface for creating,
+            reviewing, updating and deleting workouts.
+          </p>
+          <hr className="my-4"/>
+          <Link
+            to="/users/new"
+            className="btn btn-danger btn-lg custom-right-margin custom-add-margin-top">
+            Register New Account
+          </Link>
+          <button
+            onClick={this.exampleUserLogin}
+            className="btn btn-info btn-lg custom-add-margin-top">
+            View Example Account
+          </button>
+        </div>
+        <div className="custom-flex-grow-column"/>
+      </Spinner>
+    );
+  }
+}
 
 export default HomePage;
